0bf3c3efb1bf16868c1c70992299674ea8e683be
VitalijVictor <vitalijvictor@gmail.com>
2021-09-13 20:20:00 +0200

    0.3. BufferedReader. [#252489 #202008]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,22 +1,39 @@
 package ru.job4j.io;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Collections;
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
-    public static List<String> filter(String file) throws FileNotFoundException {
-        List<String> text = Collections.singletonList(new String(String.valueOf(new FileReader(file))));
-        text.stream()
-                .filter(s -> s.contains("404"))
-                .collect(Collectors.toList());
-        return text;
+    public static List<String> filter(String file) {
+        BufferedReader text = null;
+        try {
+            text = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        text
+                    .lines()
+                    .filter(s -> s.contains("404"))
+                    .collect(Collectors.toList());
+
+        return (List<String>) text;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            out.println(log);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        save(log, "404.txt");
     }
 }
