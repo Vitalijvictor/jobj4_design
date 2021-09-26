8ff3d105ae07f53954d162b874c98b50a0098833
VitalijVictor <vitalijvictor@gmail.com>
2021-09-26 14:39:00 +0200

    1. Динамический список на массиве. [#158 #200081]
    Objects.checkIndex(index, size)

--- src/main/java/ru/job4j/list/SimpleArrayList.java
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -33,7 +33,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T previous = container[index];
         container[index] = newValue;
         return previous;
@@ -41,7 +41,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T previous = container[index];
         System.arraycopy(container, index + 1, container, index, size - index - 1);
         container[--size] = null;
@@ -51,7 +51,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
