69ba14cc474a0f5f186bad704495c2f73c7c5e7d
VitalijVictor <vitalijvictor@gmail.com>
2021-09-28 13:43:26 +0200

    1. Читаем файл конфигурации [#858 #202943]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -1,11 +1,9 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
