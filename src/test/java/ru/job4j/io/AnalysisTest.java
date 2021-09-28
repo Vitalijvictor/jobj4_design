6b4ada0850a3dedeb55bfc6ac3ab3e391e2fb2bb
VitalijVictor <vitalijvictor@gmail.com>
2021-09-28 12:25:55 +0200

    Fix errors

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,13 +1,10 @@
 package ru.job4j.io;
 
-import junit.framework.TestCase;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
 import java.io.*;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -34,10 +31,9 @@ public class AnalysisTest {
     public void tempFolder() throws IOException {
         File source = folder.newFile("./data/server.log");
         File target = folder.newFile("unavailable.csv");
-        try (PrintWriter out = new PrintWriter(source)){
+        try (PrintWriter out = new PrintWriter(source)) {
             out.println("Hi Neo");
             out.println("follow white rabbit");
         }
     }
-
 }
