bddfcc06cf01fde8f5e665d49ce8159b7554c69b
VitalijVictor <vitalijvictor@gmail.com>
2021-09-28 14:27:17 +0200

    2. Анализ доступности сервера. [#859 #203010]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -12,18 +12,16 @@ public class Analysis {
                 boolean serverDown = false;
                 while (bufferedReader.ready()) {
                     String serverStatus = bufferedReader.readLine();
-                    if (!serverDown  && (serverStatus.startsWith("400"))
+                    if (!serverDown && (serverStatus.startsWith("400"))
                             || serverStatus.startsWith("500")) {
-                        printWriter.println(serverStatus.split(" ")[1]
-                                + ";");
-                        serverDown = Boolean.valueOf(serverStatus);
+                        printWriter.print(serverStatus.split(" ")[1] + ";");
+                        serverDown = true;
                     } else if (serverDown && (!serverStatus.startsWith(
                             "400"))
                             && !serverStatus.startsWith("500")) {
-                            printWriter.println(serverStatus.split(" ")[1]
-                                    + ";");
-                            serverDown = Boolean.parseBoolean(null);
-                        }
+                        printWriter.println(serverStatus.split(" ")[1] + ";");
+                        serverDown = false;
+                    }
                     }
                 } catch (IOException e) {
             e.printStackTrace();
@@ -32,12 +30,6 @@ public class Analysis {
 
 
     public static void main(String[] args) {
-//        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-//            out.println("15:01:30;15:02:32");
-//            out.println("15:10:30;23:12:32");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
         Analysis analysis = new Analysis();
         analysis.unavailable("./data/server.log", "unavailable.csv");
 
