a7cb80822023d09e110f9e5088acb78bef5dc656
VitalijVictor <vitalijvictor@gmail.com>
2021-09-29 12:47:19 +0200

    3.0. Тестирование IO [#173905 #203037]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -20,7 +20,8 @@ public class AnalysisTest {
             out.println("hello foolish dude");
             out.println("java job4j php");
         }
-        Abuse.drop(source.getAbsolutePath(), target.getAbsolutePath(), List.of("foolish", "php"));
+        Abuse.drop(source.getAbsolutePath(), target.getAbsolutePath(),
+                List.of("foolish", "php"));
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
@@ -28,12 +29,27 @@ public class AnalysisTest {
         assertThat(rsl.toString(), is("hello dude java job4j "));
     }
 
+    @Test
     public void tempFolder() throws IOException {
-        File source = folder.newFile("./data/server.log");
-        File target = folder.newFile("unavailable.csv");
+        File source = folder.newFile("source.csv");
+        File target = folder.newFile("target.csv");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("Hi Neo");
-            out.println("follow white rabbit");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is (String.join("",
+                "10:57:01;10:59:01;",
+                "11:01:02;11:02:02;"
+        )));
     }
 }
