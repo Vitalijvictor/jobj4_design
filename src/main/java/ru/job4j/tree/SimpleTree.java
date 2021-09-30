74f6f09bef61356e47172ff3649f0179905c134b
VitalijVictor <vitalijvictor@gmail.com>
2021-09-30 11:18:44 +0200

    1. Создать элементарную структуру дерева [#1711 #210036]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,13 +1,38 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
+/**
+ * Класс Node описывает узел дерева. Узел содержит хранимое значение и ссылки на дочерние узлы.
+ * @param <E>
+ */
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
     }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (condition.test(el)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+    public boolean isBinary() {
+        return !(findByPredicate((el) -> el.children.size() > 2)).isPresent();
+    }
+
 
     @Override
     public boolean add(E parent, E child) {
