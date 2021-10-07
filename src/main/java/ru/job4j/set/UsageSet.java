a001ee579d1e978b8c71ad0f61131da895642e7e
VitalijVictor <vitalijvictor@gmail.com>
2021-10-07 07:49:08 +0200

    0. Set, HashSet, TreeSet [#4954 #201797]

--- src/main/java/ru/job4j/set/UsageSet.java
+++ src/main/java/ru/job4j/set/UsageSet.java
@@ -8,11 +8,9 @@ import java.util.Set;
 public class UsageSet {
     public static void main(String[] args) {
           Set<String> strings = new HashSet<>();
-        //Set<String> strings = Set.of("one", "two", "three");  // неизменяемое
-        // множество
         strings.add("one");
         strings.add("two");
-        strings.add("two");  //дубликат
+        strings.add("two");
         strings.add("three");
         strings.addAll(List.of("one", "four", "five"));
         for (String s : strings) {
