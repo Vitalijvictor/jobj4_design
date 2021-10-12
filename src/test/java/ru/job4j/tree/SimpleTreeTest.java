28a3598e120939d394d4ebe5a77fd36b3dadd17b
VitalijVictor <vitalijvictor@gmail.com>
2021-10-12 22:05:14 +0200

    1. Создать элементарную структуру дерева [#1711 #210036]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-
+/**
+ * Test
+ */
 public class SimpleTreeTest {
     @Test
     public void when6ElFindLastThen6() {
