7b95d4f1b002bcf1a483d977db022f70772174f7
VitalijVictor <vitalijvictor@gmail.com>
2021-10-26 17:48:27 +0200

    4.2. Поиск дубликатов [#315066 #211936]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,10 +3,16 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("target/duplicatesFinder.jar"),
-                new DuplicatesVisitor());
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Path path = Files.walkFileTree(Path.of("Files"), visitor);
+        visitor.getPaths().entrySet().stream()
+                .filter(f -> f.getValue().size() > 1)
+                .collect(Collectors.toList())
+                .forEach(f -> f.getValue().stream()
+                        .forEach(System.out::println));
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,11 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    Map<FileProperty, List<Path>> paths = new HashMap<>();
+    public Map<FileProperty, List<Path>> getPaths() {
+        return paths;
+    }
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        FileProperty fileProperty = new FileProperty(attrs.size(),
+                file.getFileName().toString());
+
+        if (!paths.containsKey(fileProperty)) {
+            List<Path> duplicates = new ArrayList<>();
+            duplicates.add(file);
+            paths.put(fileProperty, duplicates);
+        } else {
+            List<Path> duplicates = paths.get(fileProperty);
+            duplicates.add(file);
+        }
         return super.visitFile(file, attrs);
     }
 }
