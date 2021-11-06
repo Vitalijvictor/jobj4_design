3efc0531d61770008753a31b33baab64a4083723
VitalijVictor <vitalijvictor@gmail.com>
2021-11-06 11:53:30 +0100

    4.2. Поиск дубликатов [#315066 #211936]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,14 +1,13 @@
 package ru.job4j.io.duplicates;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Path path = Files.walkFileTree(Path.of("Files"),
-                visitor.duplicatesFinder(visitor));
+        List<Path> paths = visitor.duplicatesFinder();
+        paths.stream().forEach(System.out::println);
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -11,14 +11,14 @@ import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     Map<FileProperty, List<Path>> paths = new HashMap<>();
+
     public Map<FileProperty, List<Path>> getPaths() {
         return paths;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProperty = new FileProperty(attrs.size(),
-                file.getFileName().toString());
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
 
         if (!paths.containsKey(fileProperty)) {
             List<Path> duplicates = new ArrayList<>();
@@ -31,14 +31,11 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
         return super.visitFile(file, attrs);
     }
 
-    public DuplicatesVisitor duplicatesFinder(DuplicatesVisitor visitor) throws IOException {
-        Path path = Files.walkFileTree(Path.of("Files"),
-                visitor);
-        visitor.getPaths().entrySet().stream()
-                .filter(f -> f.getValue().size() > 1)
-                .collect(Collectors.toList())
-                .forEach(f -> f.getValue().stream()
-                        .forEach(System.out::println));
-        return visitor;
+    public List<Path> duplicatesFinder() throws IOException {
+        Files.walkFileTree(Path.of("C:\\projects\\job4j_design\\Files"), this);
+        List<Map.Entry<FileProperty, List<Path>>> entries = this.getPaths().entrySet().stream().filter(f -> f.getValue().size() > 1).collect(Collectors.toList());
+
+        List<Path> paths = entries.stream().flatMap(f -> f.getValue().stream()).collect(Collectors.toList());
+        return paths;
     }
 }
