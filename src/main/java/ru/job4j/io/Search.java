46b825d5a3da4d74528ac1adb8361f7ba7098ab6
VitalijVictor <vitalijvictor@gmail.com>
2021-11-08 12:22:41 +0100

    5. Валидация параметров запуска. [#246865 #212144]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,26 +1,25 @@
 package ru.job4j.io;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage "
-                    + "java -jar dir.jar ROOT_FOLDER FILE_EXTENSION");
+            throw new IllegalArgumentException("Root folder is null. Usage " + "java -jar dir.jar ROOT_FOLDER FILE_EXTENSION");
         }
 
         Path start = Paths.get(args[0]);
         if (start == null) {
-            throw new IllegalArgumentException("The first argument in the "
-                    + "array is null. ");
+            throw new IllegalArgumentException("The first argument in the " + "array is null. ");
         } else {
-            search(start, p -> p.toFile().getName().endsWith("." + args[1]))
-                    .forEach(System.out::println);
+            search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
         }
     }
 
@@ -30,7 +29,8 @@ public class Search {
         return searcher.getPaths();
     }
 }
-class  SearchFiles implements FileVisitor<Path> {
+
+class SearchFiles implements FileVisitor<Path> {
     private List<Path> paths = new ArrayList<>();
     private Predicate<Path> predicate;
 
