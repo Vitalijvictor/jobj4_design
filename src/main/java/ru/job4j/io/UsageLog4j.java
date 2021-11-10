6165824f508e936fc7bf907b7d50843bf19d2d24
VitalijVictor <vitalijvictor@gmail.com>
2021-11-10 20:42:27 +0100

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,22 +1,25 @@
 package ru.job4j.io;
 
-import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * 2. Simple Loggin Facade 4 Java. [#268849 #227035]
- */
 public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Slivnojs Vitalijs";
+        String nationality = "russian";
+        String doggyName = "Rex";
+        String city = "Antwerpen";
+        String land = "Belgium";
+        byte age = 43;
+        int yearOfBirth = 1978;
+        int zipCode = 2018;
+        LOG.debug("User info name : {}, age : {}, year of birth : {}, "
+                        + "nationality : {}, zip code : {},city : {}, land : "
+                        + "{}, doggy name : {}",
+                name,
+                age, yearOfBirth, nationality, zipCode, city, land, doggyName);
     }
 }
