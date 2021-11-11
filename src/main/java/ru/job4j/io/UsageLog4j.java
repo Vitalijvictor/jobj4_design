1425e6ce28d14d4f020960e72dd0fde85a272cc8
VitalijVictor <vitalijvictor@gmail.com>
2021-11-11 08:43:22 +0100

    1. Log4j. Логирование системы. [#95335 #226945]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,25 +1,17 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Slivnojs Vitalijs";
-        String nationality = "russian";
-        String doggyName = "Rex";
-        String city = "Antwerpen";
-        String land = "Belgium";
-        byte age = 43;
-        int yearOfBirth = 1978;
-        int zipCode = 2018;
-        LOG.debug("User info name : {}, age : {}, year of birth : {}, "
-                        + "nationality : {}, zip code : {},city : {}, land : "
-                        + "{}, doggy name : {}",
-                name,
-                age, yearOfBirth, nationality, zipCode, city, land, doggyName);
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
     }
 }
