04c35febc9eaa6825fe3a7f166dd3395c3ee42b5
VitalijVictor <vitalijvictor@gmail.com>
2021-11-15 10:23:12 +0100

    Slf4j - вывод переменных. [#268852 #227168]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,18 +8,27 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
+        char a = '\u0041';
         char name = 'V';
         char nationality = 'R';
         char doggyName = 'T';
         char city = 'A';
         char land = 'B';
         byte age = 43;
+        short memCapacity = 16;
         int yearOfBirth = 1978;
         int zipCode = 2018;
-        LOG.debug("User info name : {}, age : {}, year of birth : {}, "
+        long wtf = 223372455;
+        boolean married = true;
+        float square = 10.4f;
+        double moneySafe = 5.25;
+        LOG.debug("A : {}, User info name : {}, age : {}, memCapacity : "
+                        + "{}, year " + "of " + "birth : {}, "
                         + "nationality : {}, zip code : {},city : {}, land : "
-                        + "{}, doggy name : {}",
-                name,
-                age, yearOfBirth, nationality, zipCode, city, land, doggyName);
+                        + "{}, doggy name : {}, married : {}, wtf : {}, "
+                        + "square : {}, moneySafe : {}",
+                a, name,
+                age, memCapacity, yearOfBirth, nationality, zipCode, city,
+                land, doggyName, married, wtf, square, moneySafe);
     }
 }
