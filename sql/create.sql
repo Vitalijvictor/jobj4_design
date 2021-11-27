e8734b5bb4c3f4e3d05c77393b78d420c78418c7
VitalijVictor <vitalijvictor@gmail.com>
2021-11-27 14:04:02 +0100

    0.1. Связи между таблицами [#373615 #232586]

--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,44 @@
-create table gamers(
-id serial primary key,
-	name varchar(255),
-	age int,
-	birthday date
+create table role(
+    id serial primary key,
+    function varchar(255)
 );
+create table users(
+    id serial primary key,
+    name varchar(255),
+    role_id int references role(id)
+
+);
+ create table rules(
+     id serial primary key,
+     rule varchar(255)
+ );
+ create table rules_role(
+     id serial primary key,
+     rules_id int references rules(id),
+     role_id int references role(id)
+ );
+create table item(
+     id serial primary key,
+     message varchar(255),
+     users_id int references users(id),
+     category_id int references category(id),
+     state_id int references state(id)
+ );
+create table comments(
+     comment varchar(255),
+     id serial primary key,
+     item_id int references item(id)
+ );
+ create table attachs(
+     id serial primary key,
+     new_attach varchar(255),
+     item_id int references item(id)
+ );
+create table category(
+id serial primary key,
+     category_name varchar(255)
+ );
+create table state(
+     id serial primary key,
+     position int
+ );
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,36 @@
-insert into gamers(name, age, birthday) values ('John', 25, '1986-03-15');
+insert into role(function) values ('headman');
+insert into role(function) values ('manager');
+insert into role(function) values ('assistant');
+
+insert into users(name) values ('Leny');
+insert into users(name) values ('Lian');
+insert into users(name) values ('Lens');
+
+insert into rules(rule) values ('Overall Achievement Monitor');
+insert into rules(rule) values ('Organization');
+insert into rules(rule) values ('Help');
+
+insert into item(message) values ('Academic performance');
+insert into item(message) values ('Mathematics');
+insert into item(message) values ('Social engineering');
+
+insert into comments(comment) values ('Ready');
+insert into comments(comment) values ('Not ready');
+insert into comments(comment) values ('Done');
+
+
+insert into attachs(new_attach) values ('5 pages document');
+insert into attachs(new_attach) values ('7 pages document');
+insert into attachs(new_attach) values ('2 pages document');
+
+insert into category(category_name) values ('City live');
+insert into category(category_name) values ('Country live');
+insert into category(category_name) values ('Travel');
+
+insert into state(position) values (2);
+insert into state(position) values (5);
+insert into state(position) values (4);
+
+
+
+
