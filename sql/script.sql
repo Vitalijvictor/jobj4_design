f25b169d1a62b6b0ecf15f5c5b67c941baafb4ed
VitalijVictor <vitalijvictor@gmail.com>
2021-11-28 15:10:40 +0100

    0. Select [#6861]

--- sql/script.sql
+++ sql/script.sql
@@ -1,9 +1,14 @@
-create table gamers(
-id serial primary key,
-	name varchar(255),
-	age int,
-	birthday date
-);
-insert into gamers(name, age, birthday) values ('John', 25, '1986-03-15');
-update gamers set name = 'Ludo';
-delete from gamers;
+fauna script inserts:
+
+insert into fauna(name, avg_age, discovery_date) values ('Monkey', 11, '1798-11-28');
+insert into fauna(name, avg_age, discovery_date) values ('Bear', 8, '1398-10-02');
+insert into fauna(name, avg_age, discovery_date) values ('Zebra', 16, '1698-09-12');
+insert into fauna(name, avg_age, discovery_date) values ('Cut fish', 3, '1100-04-17');
+insert into fauna(name, avg_age, discovery_date) values ('Bacillus permians', 25000, '1956-12-02');
+insert into fauna(name, avg_age, discovery_date) values ('Xestospongia muta', 50000 , null);
+
+select * from fauna where name like '%fish';
+select * from fauna where (avg_age >=10000) and (avg_age<=25000);
+select * from fauna where discovery_date = null;
+select * from fauna where discovery_date <'1950-00-0';
+
