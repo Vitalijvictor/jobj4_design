aa391302c853123af9f7bf7ff250de0e14a9fc4b
VitalijVictor <vitalijvictor@gmail.com>
2021-11-28 09:52:44 +0100

    1. Схема прав пользователи и роли. [#1093 #233377]

--- sql/create.sql
+++ sql/create.sql
@@ -8,14 +8,22 @@ create table users(
     role_id int references role(id)
 
 );
- create table rules(
+create table rules(
      id serial primary key,
      rule varchar(255)
  );
- create table rules_role(
+create table rules_role(
      id serial primary key,
      rules_id int references rules(id),
      role_id int references role(id)
+ );
+create table category(
+id serial primary key,
+     category_name varchar(255)
+ );
+create table state(
+     id serial primary key,
+     position int
  );
 create table item(
      id serial primary key,
@@ -29,16 +37,8 @@ create table comments(
      id serial primary key,
      item_id int references item(id)
  );
- create table attachs(
+create table attachs(
      id serial primary key,
      new_attach varchar(255),
      item_id int references item(id)
- );
-create table category(
-id serial primary key,
-     category_name varchar(255)
- );
-create table state(
-     id serial primary key,
-     position int
  );
