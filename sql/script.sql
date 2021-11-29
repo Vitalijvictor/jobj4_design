1e6f7621d80ac79caf3f1c9f2aea9271c6bf4d33
VitalijVictor <vitalijvictor@gmail.com>
2021-11-29 11:18:36 +0100

    1. Inner join [#373617]

--- sql/script.sql
+++ sql/script.sql
@@ -1,15 +1,58 @@
-fauna script inserts:
+create table registration_number(
+    id serial primary key,
+    state varchar(255),
+    register_number int
+);
 
-insert into fauna(name, avg_age, discovery_date) values ('Monkey', 11, '1798-11-28');
-insert into fauna(name, avg_age, discovery_date) values ('Bear', 8, '1398-10-02');
-insert into fauna(name, avg_age, discovery_date) values ('Zebra', 16, '1698-09-12');
-insert into fauna(name, avg_age, discovery_date) values ('Cut fish', 3, '1100-04-17');
-insert into fauna(name, avg_age, discovery_date) values ('Bacillus permians', 25000, '1956-12-02');
-insert into fauna(name, avg_age, discovery_date) values ('Xestospongia muta', 50000 , null);
+create table vehicle(
+    id serial primary key,
+    brand varchar(255),
+	model varchar(255),
+	year_of_issue int,
+    registration_number_id int references registration_number(id) unique
+);
 
-select * from fauna where name like '%fish';
-select * from fauna where avg_age between 10000
-and 25000;
-select * from fauna where discovery_date is null;
-select * from fauna where discovery_date <'1950-00-0';
+insert into registration_number(state,
+register_number) values ('Alabama', 1445412);
+insert into registration_number(state,
+register_number) values ('Dacota', 1588656);
+insert into registration_number(state,
+register_number) values ('Virginia', 1475555);
+insert into registration_number(state,
+register_number) values ('New York', 2545523);
+insert into registration_number(state,
+register_number) values ('Montana', 556621);
+insert into registration_number(state,
+register_number) values ('Dacota', 5454988);
 
+
+insert into vehicle(brand,
+model, year_of_issue, registration_number_id)
+values ('DMC', 'F12', 1956, 2);
+insert into vehicle(brand,
+model, year_of_issue, registration_number_id)
+values ('BMW', '530', 2011, 3);
+insert into vehicle(brand,
+model, year_of_issue, registration_number_id)
+values ('Mercedes', 'E220', 2017, 1);
+insert into vehicle(brand,
+model, year_of_issue, registration_number_id)
+values ('Opel', 'Zafira', 2000, 4);
+
+insert into vehicle(brand,
+model, year_of_issue)
+values ('Honda', 'Crv', 2015);
+insert into vehicle(brand,
+model, year_of_issue)
+values ('Toyota', 'Prius', 2014);
+
+select * from vehicle inner join
+registration_number r on vehicle
+.registration_number_id = r.id;
+
+select * from vehicle join registration_number
+r on vehicle.registration_number_id = r.id;
+
+select  rr.brand, r.register_number, r.state
+from vehicle as rr join registration_number as
+r on rr.registration_number_id = r.id;
